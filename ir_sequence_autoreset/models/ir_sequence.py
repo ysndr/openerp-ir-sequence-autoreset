c2e49af64205ae8eb53605fa8d697249f7989661
Yannik Sander <y.sander@batt-gmbh.com>
2017-03-28 16:56:22 +0200

    modifications to make it work with odoo v9

--- ir_sequence_autoreset/models/ir_sequence.py
+++ ir_sequence_autoreset/models/ir_sequence.py
@@ -19,8 +19,12 @@
 #
 ##############################################################################
 
-from odoo import fields, models,  _
-from odoo.exceptions import UserError
+from openerp import fields, models,  _
+from openerp.exceptions import UserError
+from datetime import datetime
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 def _alter_sequence(cr, seq_name, number_increment=None, number_next=None):
@@ -38,6 +42,8 @@ def _alter_sequence(cr, seq_name, number_increment=None, number_next=None):
         statement += " RESTART WITH %d" % (number_next, )
     cr.execute(statement)
 
+    _logger.debug("statement (`{}`) executed".format(statement))
+
 
 def _select_nextval(cr, seq_name):
     cr.execute("SELECT nextval('%s')" % seq_name)
@@ -51,14 +57,45 @@ def _update_nogap(self, number_increment):
     self.invalidate_cache(['number_next'], [self.id])
     return number_next
 
+def _extract_timestamp(reset_time):
+    reset_timestamp = None
+    try:
+        _logger.debug("reset_timestamp: {}".format(reset_time.split(":", 1)[1]))
+        reset_timestamp = datetime.strptime(reset_time.split(":", 1)[1], "%Y-%m-%d %H:%M:%S.%f")
+    except ValueError:
+        reset_timestamp = datetime.min
+
+    return reset_timestamp
+
+
+def _exeeds_period(current_time, reset_time):
+    current_period, _ = current_time
+    reset_period, _ = reset_time
+
+    if current_period != reset_period: return True
+
+    _, current_timestamp = current_time
+    _, reset_timestamp = reset_time
+
+    _logger.debug("current_timestamp: {}; reset_timestamp {}".format(current_timestamp, reset_timestamp))
+
+    if (current_timestamp.year > reset_timestamp.year) \
+    or (current_timestamp.month > reset_timestamp.month and current_period in dict(_reset_periods[1:])) \
+    or (current_timestamp.isocalendar()[1] > reset_timestamp.isocalendar()[1] and current_period in dict(_reset_periods[2:])) \
+    or (current_timestamp.day > reset_timestamp.day and current_period in dict(_reset_periods[3:])) \
+    or (current_timestamp.hour > reset_timestamp.hour and current_period in dict(_reset_periods[4:])) \
+    or (current_timestamp.minute > reset_timestamp.minute and current_period in dict(_reset_periods[5:])) \
+    or (current_timestamp.second > reset_timestamp.second and current_period in dict(_reset_periods[6:])): return True
+
+_reset_periods = [('year', 'Every Year'), ('month', 'Every Month'), ('woy', 'Every Week'), ('day', 'Every Day'),
+ ('h24', 'Every Hour'), ('min', 'Every Minute'), ('sec', 'Every Second')]
 
 class IrSequence(models.Model):
     _inherit = 'ir.sequence'
 
     auto_reset = fields.Boolean('Auto Reset', default=False)
     reset_period = fields.Selection(
-        [('year', 'Every Year'), ('month', 'Every Month'), ('woy', 'Every Week'), ('day', 'Every Day'),
-         ('h24', 'Every Hour'), ('min', 'Every Minute'), ('sec', 'Every Second')],
+        _reset_periods,
         'Reset Period', required=True, default='month')
     reset_time = fields.Char('Name', size=64, help="")
     reset_init_number = fields.Integer('Reset Number', default=1, required=True, help="Reset number of this sequence")
@@ -66,9 +103,14 @@ class IrSequence(models.Model):
     def _next_do(self):
         if self.implementation == 'standard':
             if self.auto_reset:
-                current_time = ':'.join([self.reset_period, self.reset_period])
-                if current_time != self.reset_time:
-                    self._cr.execute("UPDATE ir_sequence SET reset_time=%s WHERE id=%s ", (current_time, self.id))
+                current_time = (self.reset_period, datetime.today())
+                reset_time = (self.reset_time.split(":")[0], _extract_timestamp(self.reset_time))
+
+                _logger.debug("current time {}; reset_time {}".format(current_time, reset_time))
+
+                if _exeeds_period(current_time, reset_time):
+                    _logger.debug("saved time: {}".format(':'.join((current_time[0], str(current_time[1])))))
+                    self._cr.execute("UPDATE ir_sequence SET reset_time=%s WHERE id=%s ", (':'.join((current_time[0], str(current_time[1]))), self.id))
                     _alter_sequence(self._cr, "ir_sequence_%03d" % self.id, self.number_increment, self.reset_init_number)  # NoQA
                     self._cr.commit()
             number_next = _select_nextval(self._cr, 'ir_sequence_%03d' % self.id)
@@ -82,9 +124,14 @@ class IrSequenceDateRange(models.Model):
 
     def _next(self):
         if self.sequence_id.implementation == 'standard':
-            if self.auto_reset:
-                current_time = ':'.join([self.reset_period, self.reset_period])
-                if current_time != self.reset_time:
+            if self.sequence_id.auto_reset:
+
+                current_time = (self.sequence_id.reset_period, datetime.today())
+                reset_time = (self.sequence_id.reset_time.split(":")[0], _extract_timestamp(self.sequence_id.reset_time))
+
+                _logger.debug("current time {}; reset_time {}".format(current_time, reset_time))
+
+                if _exeeds_period(current_time, reset_time):
                     self._cr.execute("UPDATE ir_sequence SET reset_time=%s WHERE id=%s ", (current_time, self.id))
                     _alter_sequence(self._cr, "ir_sequence_%03d" % self.id, self.number_increment, self.reset_init_number)  # NoQA
                     self._cr.commit()
